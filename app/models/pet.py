2f9e150e8e55d76d09ebab3a1c7ab1af839f7d94
Adrian Prado <adrian.prado@adadevelopersacademy.org>
2024-12-05 17:05:42 -0600

    Update pet model

--- app/models/pet.py
+++ app/models/pet.py
@@ -3,7 +3,7 @@ from ..db import db
 
 class Pet(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str]
+    possible_names: Mapped[list[str]]
     animal_type: Mapped[str]
     personality: Mapped[str]
     color: Mapped[str]
@@ -11,7 +11,7 @@ class Pet(db.Model):
 def to_dict(self):
         return {
             "id" : self.id,
-            "name" : self.name,
+            "possible names": self.possible_names,
             "animal type": self.animal_type,
             "personality": self.personality,
             "coloring": self.color
