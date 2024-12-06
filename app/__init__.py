3846a1710a029fea07b2d2275d1574f809318fb3
Adrian Prado <adrian.prado@adadevelopersacademy.org>
2024-12-06 09:09:42 -0600

    Update configuration in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,21 @@
 import os
 from flask import Flask
-from .routes.pet_routes import bp
+from .routes.pet_routes import bp as pets_bp
 from .db import db, migrate
 from .models import pet
 
-def create_app(test_config=None):
+def create_app(config=None):
+    # __name__ stores the name of the module we're in
     app = Flask(__name__)
 
-    if not test_config:
-        db_to_use = os.environ.get("SQLALCHEMY_DATABASE_URI")
-    else:
-        db_to_use = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
-    
-
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_to_use
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    if config:
+        app.config.update(config)
 
     db.init_app(app)
     migrate.init_app(app, db)
 
-    app.register_blueprint(bp)
+    app.register_blueprint(pets_bp)
 
     return app
