feef8083bc921b66b77595055ee78504b199730d
Adrian Prado <adrian.prado@adadevelopersacademy.org>
2024-12-06 09:11:26 -0600

    Update pet model. Made name optional and added from_dict()

--- app/models/pet.py
+++ app/models/pet.py
@@ -1,18 +1,33 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from typing import Optional
 from ..db import db
 
 class Pet(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    possible_names: Mapped[list[str]]
+    name: Mapped[Optional[str]]
     animal_type: Mapped[str]
     personality: Mapped[str]
     color: Mapped[str]
 
-def to_dict(self):
-        return {
+    def to_dict(self):
+        pet_dict = {
             "id" : self.id,
-            "possible names": self.possible_names,
-            "animal type": self.animal_type,
+            "animal": self.animal_type,
             "personality": self.personality,
-            "coloring": self.color
+            "coloration": self.color
         }
+        if self.name:
+            pet_dict["name"] = self.name
+        
+        return pet_dict
+
+
+    @classmethod
+    def from_dict(cls, data_dict):
+        new_pet = cls(
+            animal_type = data_dict["animal"],
+            personality = data_dict["personality"],
+            color = data_dict["coloration"]
+        )
+
+        return new_pet
