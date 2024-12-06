66174e2f74f087586f21f4716ace68811a643181
Adrian Prado <adrian.prado@adadevelopersacademy.org>
2024-12-06 09:12:58 -0600

    Added post and get to pet routes

--- app/routes/pet_routes.py
+++ app/routes/pet_routes.py
@@ -5,5 +5,26 @@ from ..models.pet import Pet
 bp = Blueprint("pets", __name__, url_prefix="/pets")
 
 @bp.post("")
-def add_pets():
-    pass
+def create_pet():
+    request_body = request.get_json()
+    try: 
+        new_pet = Pet.from_dict(request_body)
+        db.session.add(new_pet)
+        db.session.commit()
+
+        return make_response(new_pet.to_dict(), 201)
+    
+    except KeyError as e:
+        abort(make_response({"message": f"missing required value: {e}"}, 400))
+
+@bp.get("")
+def get_pets():
+    pet_query = db.select(Pet)
+
+    pets = db.session.scalars(pet_query)
+    response = []
+
+    for pet in pets:
+        response.append(pet.to_dict())
+
+    return jsonify(response)
